edead1ab08119d884514c634eb57febc9005c0c7
Shyam Venkata Rama Krishna <2486130@cognizant.com>
2026-04-09 17:12:37 +0530

    Changes made

--- pes-service/src/main/java/com/cognizant/pes/PesApplication.java
+++ pes-service/src/main/java/com/cognizant/pes/PesApplication.java
@@ -2,8 +2,10 @@ package com.cognizant.pes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PesApplication {
 
 	public static void main(String[] args) {
