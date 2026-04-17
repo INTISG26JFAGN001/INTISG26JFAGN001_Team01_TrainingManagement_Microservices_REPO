cd62f1548aaae4446232ada2009e7f30dbc8d3f5
Shyamduba <Duba.ShyamVenkataRamaKrishna@cognizant.com>
2026-04-17 11:43:13 +0530

    added swagger documentation

--- pes-service/src/main/java/com/cognizant/pes/dto/response/EvaluationResponseDTO.java
+++ pes-service/src/main/java/com/cognizant/pes/dto/response/EvaluationResponseDTO.java
@@ -2,6 +2,7 @@ package com.cognizant.pes.dto.response;
 
 public record EvaluationResponseDTO(
         Long id,
+        Long batchId,
         Long associateId,
         Double interimScore,
         Double finalScore,
